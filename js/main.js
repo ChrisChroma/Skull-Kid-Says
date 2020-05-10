00a119f61bda72585d470a7fa61138572ba1b3f6
ChrisChroma <chrischroma1030@gmail.com>
2020-05-10 08:37:14 -0500

    set up

--- js/main.js
+++ js/main.js
@@ -1,4 +1,46 @@
 // Constants
+const color = {
+    1: 'red',
+    2: 'green',
+    3: 'blue',
+    4: 'yellow'
+}
+
+const theme = [
+  {
+    name: "lostWoods",
+    background: "https://i.ytimg.com/vi/ecnb7qOl4Pc/maxresdefault.jpg",
+  },
+  {
+    name: "clockTown",
+    background:
+      "https://i.pinimg.com/originals/d7/7f/8c/d77f8c43dec780ed972d3baf73471136.jpg",
+  },
+  {
+    name: "songOfStorms",
+    background: "https://i.pinimg.com/originals/6b/90/9e/6b909e88b59dab6b460c05d4e1334f4a.jpg",
+  },
+];
+
 // Cache Element References
+const power = document.getElementById('red');
+const courage = document.getElementById('green');
+const wisdom = document.getElementById('blue');
+const skullKid = document.getElementById('yellow')
+const start = document.querySelector('#start');
+
 // Event Listeners
+
 // Functions
+
+init();
+
+function init(){
+
+}
+
+function render () {
+
+}
+
+render();
