05b94c3a861b6bcb2176aeaa9ed4f705105582f0
ChrisChroma <chrischroma1030@gmail.com>
2020-05-15 04:13:55 -0500

    Theme menu Functions

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let round = 0;
 getComputerSelection(round, computerChoice);
 let playerChoice = [];
 let themeChoice = [];
+let bgMusic;
 
 const themes = [
   {
@@ -17,13 +18,13 @@ const themes = [
     name: "clockTown",
     music: new Audio("audio/Clock Town Day 1.mp3"),
     background:
-      "url(https://i.pinimg.com/originals/d7/7f/8c/d77f8c43dec780ed972d3baf73471136.jpg)",
+      "url(https://pbs.twimg.com/media/CTyCureUkAAjvkW?format=jpg&name=large)",
   },
   {
     name: "songOfStorms",
     music: new Audio("audio/Song of Storms SSBU.mp3"),
     background:
-      "url(https://i.pinimg.com/originals/6b/90/9e/6b909e88b59dab6b460c05d4e1334f4a.jpg)",
+      "url(https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/6c0272ad-8c88-47cf-aa76-21663d92348e/ddwofoa-723e436d-bd18-414e-94cd-413dcd068636.jpg/v1/fill/w_1192,h_670,q_70,strp/song_of_storms_by_jasqreate_ddwofoa-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD02NzUiLCJwYXRoIjoiXC9mXC82YzAyNzJhZC04Yzg4LTQ3Y2YtYWE3Ni0yMTY2M2Q5MjM0OGVcL2Rkd29mb2EtNzIzZTQzNmQtYmQxOC00MTRlLTk0Y2QtNDEzZGNkMDY4NjM2LmpwZyIsIndpZHRoIjoiPD0xMjAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.-RE33Zujpg-7PODWKCultGWKxrB3WEci1WrFbuKABTo)",
   },
 ];
 
@@ -35,7 +36,9 @@ const start = document.getElementById("start");
 const menu = document.getElementById("themeMenu");
 const showMenu = document.getElementById("showMenu");
 const themeMenu = document.getElementById("themeMenu");
-const themeBtn = document.querySelectorAll(".theme");
+const lostWoodsTheme = document.getElementById("lostWoods");
+const clockTownTheme = document.getElementById("clockTown");
+const songOfStormsTheme = document.getElementById("songOfStorms");
 
 // Event Listeners
 gamePieces.forEach(function (element) {
@@ -67,15 +70,22 @@ gamePieces.forEach(function (element) {
 
 start.addEventListener("click", function () {
   play(round, computerChoice, gamePieces);
+  playBgMusic(themeChoice);
 });
 
-themeBtn.addEventListener("click", function (element) {
-  themeChoice.push(event.target.id);
-  console.log("themeChoice", themeChoice);
-  let themeChoices = [];
-  themeChoice.forEach(function (theme) {
-    themeChoices.push(themes.indexOf(theme));
-  });
+lostWoodsTheme.addEventListener("click", function () {
+  pauseBgMusic();
+  selectTheme(0);
+});
+
+clockTownTheme.addEventListener("click", function () {
+  pauseBgMusic();
+  selectTheme(1);
+});
+
+songOfStormsTheme.addEventListener("click", function () {
+  pauseBgMusic();
+  selectTheme(2);
 });
 
 // Functions
@@ -134,24 +144,24 @@ function reset() {
   playerChoice = [];
 }
 
-function selectTheme(themeChoice) {
-  // let music = theme.music;
-  // music.pause();
-  changeBg(themeChoice);
-  playBgMusic(themeChoice);
+function selectTheme(theme) {
+  bgMusic = themes[theme].music;
+  bgMusic.pause();
+  changeBg(theme);
 }
 
-function changeBg(themeChoice) {
-  body.style.backgroundImage = themes[themeChoice].background;
+function changeBg(theme) {
+  body.style.backgroundImage = themes[theme].background;
 }
 
-function playBgMusic(themeChoice) {
-  themes[themeChoice].music.volume = 0.05;
-  // theme[themeChoice].music.loop = true;
-  themes[themeChoice].music.play();
-  setTimeout(function () {
-    themes[themeChoice].music.pause();
-  }, 40000);
+function pauseBgMusic() {
+  bgMusic.pause();
+}
+
+function playBgMusic() {
+  bgMusic.volume = 0.05;
+  bgMusic.loop = true;
+  bgMusic.play();
 }
 
 function init() {
